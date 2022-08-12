0f94c519240901a8fdc218cb2c32600c9f9da062
Mike BITA BENI <45491403+mikebita23@users.noreply.github.com>
2022-08-12 21:22:36 +0200

    First Docker
    My image first Docker

---
+++ assets/js/colors.js
@@ -0,0 +1,18 @@
+window.colors = {
+    'primary-color': '#1E88E5',
+    'secondary-color': '#082a48',
+    'info-color': '#006f7d',
+    'danger-color': '#E53935',
+    'success-color': '#347c37',
+    'warning-color': '#cc7a00',
+    'grey-200': '#EEEEEE',
+	'grey-300': '#E0E0E0',
+	'grey-400': '#BDBDBD',
+	'grey-500': '#9E9E9E',
+	'grey-900': '#212121',
+	'red-300': '#E57373',
+	'purple-500': '#9C27B0',
+    'chart-primary': '#E53935',
+    'chart-secondary': '#1E88E5',
+    'chart-third': '#347c37'
+};
